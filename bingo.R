0811efdac8c4641c72d3e3ea5c54e1c3d26a6c0d
“ncpollock” <noah.pollock@yahoo.com>
2019-07-10 21:44:32 -0400

    random sample instead of sequential sample, added landscape

--- bingo.R
+++ bingo.R
@@ -4,6 +4,10 @@
     # identify sets eg all B, I, row 1, 2, and diagonals
     # force unique combinations
 
+# I should use sample and pull out 25 without replacement each time
+# do this twice as many times as needed?
+# then remove the boards with more than one winner at a time?
+
 library(dplyr)
 library(stringr)
 library(ggplot2)
@@ -11,12 +15,11 @@ library(ggplot2)
 library(png)
 library(grid)
 
-bingo_df <- read.csv(
-  "C:/Users/User/Downloads/bingo.csv"
+bingo_df <- read.csv("bingo.csv"
   , stringsAsFactors = FALSE
 )
 
-heart <- readPNG("D:/heart.png")
+heart <- readPNG("www/heart.png")
 g <- rasterGrob(heart, interpolate=TRUE)
 
 tiles <- 25
@@ -43,12 +46,17 @@ ext_bingo_df <- do.call(rbind,lapply(1:8,function(x) bingo_df <- bind_rows(bingo
 plot_list = list()
 
 for(i in 1:guests){
-plot_df <- grid_df %>%
-  mutate(gift = str_wrap(ext_bingo_df$Gift_Title[1:25],width = 14)) %>%
-  # force 13 to be a free space, separate variable to give it unique aesthetics
-  mutate(gift_free = ifelse(x==3 & y==3,sprintf("\u2665"),NA) 
-         , gift = ifelse(x==3 & y==3,NA,gift))
+# plot_df <- grid_df %>%
+#   mutate(gift = str_wrap(ext_bingo_df$Gift_Title[1:25],width = 14)) %>%
+#   # force 13 to be a free space, separate variable to give it unique aesthetics
+#   mutate(gift_free = ifelse(x==3 & y==3,sprintf("\u2665"),NA) 
+#          , gift = ifelse(x==3 & y==3,NA,gift))
 
+plot_df <- grid_df %>%
+  mutate(gift = str_wrap(sample(bingo_df$Gift_Title,25),width = 14)) %>%
+  # force 13 to be a free space, separate variable to give it unique aesthetics
+  mutate(gift = ifelse(x==3 & y==3,NA,gift))
+  
 temp_plot <- ggplot(plot_df, aes(x, y, width = w)) +
   geom_tile(color = "black",fill=NA) +
   geom_text(aes(label=gift)) +
@@ -85,7 +93,7 @@ ext_bingo_df <- ext_bingo_df %>%
 # ggsave("D:/bingo_two_per_page.pdf", gridExtra::marrangeGrob(grobs = plot_list, nrow=2, ncol=1,top = NULL)
 #        , device=cairo_pdf)
 
-ggsave("D:/bingo_one_per_page.pdf", gridExtra::marrangeGrob(grobs = plot_list, nrow=1, ncol=1,top = NULL))
+ggsave("bingo_one_per_page.pdf", gridExtra::marrangeGrob(grobs = plot_list, nrow=1, ncol=1,top = NULL))
 
 # # save all BINGO boards to one big pdf
 # pdf("bingo.pdf")
@@ -97,7 +105,7 @@ ggsave("D:/bingo_one_per_page.pdf", gridExtra::marrangeGrob(grobs = plot_list, n
 # SANDBOX #########################################################################
 
 
-ext_bingo_df %>%
+test_df <- ext_bingo_df %>%
   mutate(
     board_id = rep(1:nrow(ext_bingo_df),length.out = nrow(ext_bingo_df),each=25)
     , column = rep(c("B","I","N","G","O"),length.out = nrow(ext_bingo_df))
@@ -114,5 +122,4 @@ ext_bingo_df %>%
     , column == "G" & row == 2 ~ 'BT'
     , column == "O" & row == 1 ~ 'BT'
   ))
-
 
